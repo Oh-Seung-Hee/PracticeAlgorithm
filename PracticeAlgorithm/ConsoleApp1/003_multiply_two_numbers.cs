85333c62c889e0798d99fe8959c50dae80adfc36
OHSEUNG <dhtmdgml@knou.ac.kr>
2024-01-13 00:02:12 +0900

    두 수의 곱

---
+++ PracticeAlgorithm/ConsoleApp1/003_multiply_two_numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+/// <summary>
+/// <두 수의 곱>
+/// [문제 설명]
+/// 정수 num1, num2가 매개변수로 주어집니다. num1과 num2를 곱한 값을 return하도록 solution 함수를 완성해주세요.
+/// [제한 사항]
+/// 0 ≤ num1 ≤ 100
+/// 0 ≤ num2 ≤ 100
+/// </summary>
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = 0;
+
+        if (num1 >= 0 && num1 <= 100 && num2 >= 0 && num2 <= 100)
+        {
+            answer = num1 * num2;
+        }
+
+        return answer;
+    }
+}
